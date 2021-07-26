17e699d035577c4af0940f3b26d972bf5a11cd5c
Jagveer-Sangha <jagveersangha@gmail.com>
2021-07-25 23:03:22 -0400

    Added HawkHacksIcon spinning animation to :hover

--- src/App.jsx
+++ src/App.jsx
@@ -32,19 +32,32 @@ const HawkHacksIcon = Styled.img`
     :hover {
         filter: brightness(75%);
         transition: all 0.25s ease;
+        
+        animation-name:rotate;
+        animation-duration: 5s;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+
+        @keyframes rotate{
+            100%{
+                transform:rotate(360deg);
+            }
+        }
+
     }
+    
     width: 5rem;
     ${breakpoints("width", [
-        { 250: "4rem" }
-    ])}
+    { 250: "4rem" }
+])}
     height: 5rem;
     ${breakpoints("height", [
-        { 250: "4rem" }
-    ])}
+    { 250: "4rem" }
+])}
     margin: 3rem 0;
     ${breakpoints("margin", [
-        { 250: "2rem 0"}
-    ])}
+    { 250: "2rem 0" }
+])}
 `;
 
 const PanelDiv = Styled.div`
@@ -55,33 +68,33 @@ const PanelDiv = Styled.div`
     height: max-content;
     width: max-content;
     ${breakpoints("width", [
-        { 600: "22rem" },
-        { 450: "20rem" },
-        { 350: "calc(100vw - 2rem)" }
-    ])};
+    { 600: "22rem" },
+    { 450: "20rem" },
+    { 350: "calc(100vw - 2rem)" }
+])};
     padding: 2rem 4rem;
     ${breakpoints("padding", [
-        { 800: "2rem 3rem" },
-        { 600: "2rem 2rem" },
-        { 450: "1.6rem 1.2rem" },
-        { 350: "1.2rem 0.6rem" }
-    ])}
+    { 800: "2rem 3rem" },
+    { 600: "2rem 2rem" },
+    { 450: "1.6rem 1.2rem" },
+    { 350: "1.2rem 0.6rem" }
+])}
     margin-bottom: 2rem;
     position: relative;
     p {
     font-size: 1rem;
     ${breakpoints("font-size", [
-        { 450: "0.9rem" },
-        { 350: "0.82rem" },
-        { 250: "0.8rem" }
-    ])}
+    { 450: "0.9rem" },
+    { 350: "0.82rem" },
+    { 250: "0.8rem" }
+])}
     }
 `;
 
 const PanelBackground = Styled.img`
     opacity: 0.14;
     position: absolute;
-    left: 15%;
+    left: 14%;
     top: 0;
     width: 70%;
     height: 100%;
@@ -93,8 +106,8 @@ const SocialMediaDiv = Styled.div`
     margin-bottom: 1.6rem;
     flex-direction: row;
     ${breakpoints("flex-direction", [
-        { 300: "column" }
-    ])}
+    { 300: "column" }
+])}
 `;
 
 const SocialMediaIcon = Styled.img`
@@ -110,21 +123,21 @@ const Title = Styled.h1`
     margin: 2rem 0 0 0;
     font-size: 2.2rem;
     ${breakpoints("font-size", [
-        { 600: "2.2rem" },
-        { 450: "1.8rem" },
-        { 300: "1.4rem" },
-        { 250: "1.1rem" }
-    ])}
+    { 600: "2.2rem" },
+    { 450: "1.8rem" },
+    { 300: "1.4rem" },
+    { 250: "1.1rem" }
+])}
 `;
 
 const Subtitle = Styled.h3`
     font-weight: 600;
     font-size: 1.2rem;
     ${breakpoints("font-size", [
-        { 600: "1rem" },
-        { 300: "0.9rem" },
-        { 250: "0.8rem" }
-    ])}
+    { 600: "1rem" },
+    { 300: "0.9rem" },
+    { 250: "0.8rem" }
+])}
     margin: 0.4rem 0 1.2rem 0;
 `;
 
@@ -137,17 +150,17 @@ const ClockPiece = Styled.div`
     h4 {
     font-size: 1.35rem;
     ${breakpoints("font-size", [
-        { 300: "1rem" },
-        { 250: "0.9rem" }
-    ])}
+    { 300: "1rem" },
+    { 250: "0.9rem" }
+])}
     margin: 0;
     }
     p {
     font-size: 0.75rem;
     ${breakpoints("font-size", [
-        { 300: "0.6rem" },
-        { 250: "0.56rem" }
-    ])}
+    { 300: "0.6rem" },
+    { 250: "0.56rem" }
+])}
     margin: 0;
     }
 `;
@@ -166,10 +179,10 @@ const FormInput = Styled.input`
     border-radius: 0.8rem;
     font-size: 1rem;
     ${breakpoints("font-size", [
-        { 450: "0.9rem" },
-        { 300: "0.84rem" },
-        { 250: "0.8rem" }
-    ])}
+    { 450: "0.9rem" },
+    { 300: "0.84rem" },
+    { 250: "0.8rem" }
+])}
     padding: 0.6rem;
     margin: 0.2rem 0;
 `;
@@ -181,10 +194,10 @@ const FormButton = Styled.button`
     border-radius: 0.8rem;
     font-size: 1rem;
     ${breakpoints("font-size", [
-        { 450: "0.9rem" },
-        { 300: "0.84rem" },
-        { 250: "0.8rem" }
-    ])}
+    { 450: "0.9rem" },
+    { 300: "0.84rem" },
+    { 250: "0.8rem" }
+])}
     padding: 0.6rem 1.2rem;
     margin: 0.8rem 0 0.4rem 0;
     flex-grow: 1;
@@ -200,8 +213,8 @@ const FormInputDiv = Styled.div`
     display: flex;
     flex-direction: row;
     ${breakpoints("flex-direction", [
-        { 450: "column" }
-    ])}
+    { 450: "column" }
+])}
     marginTop: 2rem;
 `;
 
@@ -301,7 +314,7 @@ class SocialMediaBar extends React.Component {
                 <a href="https://discord.gg/z8XbEEXkqN" target="_blank" rel="noreferrer"><SocialMediaIcon as={DiscordIcon} alt="Discord Icon" /></a>
                 <a href="" target="" rel="noreferrer"><SocialMediaIcon as={FacebookIcon} alt="Facebook Icon" /></a>
                 <a href="https://twitter.com/wluhawkhacks" target="_blank" rel="noreferrer"><SocialMediaIcon as={TwitterIcon} alt="Twitter Icon" /></a>
-                <a href="https://www.instagram.com/wluhawkhacks/" target="_blank" rel="noreferrer"><SocialMediaIcon as={InstagramIcon} alt="Instagram Icon"/></a>
+                <a href="https://www.instagram.com/wluhawkhacks/" target="_blank" rel="noreferrer"><SocialMediaIcon as={InstagramIcon} alt="Instagram Icon" /></a>
                 <a href="https://github.com/LaurierHawkHacks" target="_blank" rel="noreferrer"><SocialMediaIcon as={GithubIcon} alt="Github Icon" /></a>
             </SocialMediaDiv>
         );
@@ -326,7 +339,7 @@ class ClockWidget extends React.Component {
         const hours = ((countdown % 86400000) - (countdown % 3600000)) / 3600000;
         const minutes = ((countdown % 3600000) - (countdown % 60000)) / 60000;
         const seconds = ((countdown % 60000) - (countdown % 1000)) / 1000;
-        
+
         return (
             <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
                 <ClockPiece>
