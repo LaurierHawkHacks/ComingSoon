4155c496ed63a1700c857db4dd175d4a67c36a4c
Benjamin Sengupta <benjaminsengupta@gmail.com>
2022-03-16 22:50:54 -0400

    Add MLH Badge

--- src/App.jsx
+++ src/App.jsx
@@ -398,6 +398,29 @@ function App() {
                             <HawkHacksIcon src={Icon} alt="HawkHacks Icon" />
                         </a>
 
+                        <a
+                            id="mlh-trust-badge"
+                            style={{
+                                display: "block",
+                                maxWidth: "100px",
+                                minWidth: "60px",
+                                position: "fixed",
+                                right: "50px",
+                                top: 0,
+                                width: "10%",
+                                zIndex: 10000
+                            }}
+                            href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=blue"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <img
+                                src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-blue.svg"
+                                alt="Major League Hacking 2022 Hackathon Season"
+                                style={{ width: "100%" }}
+                            />
+                        </a>
+
                         <SpaceFillerDiv />
                         <PanelDiv className="dropshadow-large">
                             <PanelBackground src={Icon} />
