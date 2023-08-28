6152773bdf1546dd1b087612e707610461c05b22
Nausher Rao <SherRao15@gmail.com>
2023-08-28 18:17:46 -0400

    Finish social media bar

--- src/components/SocialMediaBar.tsx
+++ src/components/SocialMediaBar.tsx
@@ -1,32 +1,41 @@
-import React, { ReactNode } from "react";
+import React, { ReactElement } from "react";
 import {
     RiDiscordLine as DiscordIcon,
-    RiTwitterLine as TwitterIcon,
+    RiLinkedinLine as LinkedinIcon,
     RiInstagramLine as InstagramIcon,
     RiGithubLine as GithubIcon
 } from "react-icons/ri";
 
 interface SocialMediaIconProps {
-    icon: ReactNode;
+    icon: ReactElement;
     url: string;
 }
 
 const SocialMediaBar = () => {
+    const iconProps = {size: 50, className: "social-icon"};
+
     return (
         <div className="social-container">
-            <SocialMediaIcon url="https://discord.gg/z8XbEEXkqN" icon={DiscordIcon} />
-            <SocialMediaIcon url="https://twitter.com/wluhawkhacks" icon={TwitterIcon} />
-            <SocialMediaIcon url="https://instagram.com/wluhawkhacks/" icon={InstagramIcon} />
-            <SocialMediaIcon url="https://github.com/LaurierHawkHacks" icon={GithubIcon} />
+            <SocialMediaIcon url="https://discord.gg/z8XbEEXkqN"
+                icon={<DiscordIcon {...iconProps} />}
+            />
+            <SocialMediaIcon url="https://linkedin.com/company/hawkhacks"
+                icon={<LinkedinIcon {...iconProps} />}
+            />
+            <SocialMediaIcon url="https://instagram.com/wluhawkhacks/"
+                icon={<InstagramIcon {...iconProps} />}
+            />
+            <SocialMediaIcon url="https://github.com/LaurierHawkHacks"
+                icon={<GithubIcon {...iconProps} />}
+            />
         </div>
     );
 };
 
 const SocialMediaIcon = ({icon, url}: SocialMediaIconProps) => {
-    const IconComponent = icon;
     return (
         <a href={url} target="_blank" rel="noreferrer">
-            <IconComponent size={50} className="social-icon" />
+            {icon}
         </a>
     );
 };
