e47c21da54d0a37a693c9babe57a9cd45e264a5c
Nausher Rao <SherRao15@gmail.com>
2023-08-29 13:18:04 -0400

    Refactor alias imports

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,7 @@ export default defineConfig({
             "@": path.resolve(__dirname, "src/"),
             "@assets": path.resolve(__dirname, "src/assets/"),
             "@components": path.resolve(__dirname, "src/components/"),
-            "@pages": path.resolve(__dirname, "src/pages/"),
-            "@utils": path.resolve(__dirname, "src/utils/"),
-            "@hooks": path.resolve(__dirname, "src/utils/hooks/"),
-            "@constants": path.resolve(__dirname, "src/utils/constants/"),
-            "@notifications": path.resolve(__dirname, "src/utils/notifications"),
+            "@styles": path.resolve(__dirname, "src/styles/")
         }
     }
 });
