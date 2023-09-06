62a0b59fa73ba025027db771563076247fe9dd10
Nausher Rao <SherRao15@gmail.com>
2023-09-06 17:18:02 -0400

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Icon from "@assets/icon.svg";
-import { Card, Clock, Clouds, Spacer, SocialMediaBar, Buttons } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Card, Clock, Spacer, SocialMediaBar, Buttons } from "./components";
 
 interface ContainerProps {
     children: React.ReactNode;
@@ -8,37 +9,48 @@ interface ContainerProps {
 
 const App = () => {
     return (
-        <AppContainer>
-            <ContentContainer>
-                {/* <Clouds /> */}
-
-                <a href="https://www.youtube.com/watch?v=cDj2r8QEzzk" target="_blank" rel="noreferrer">
-                    <img className="icon" src={Icon} />
-                </a>
-
-                <Card>
-                    <Clock />
-                    <Spacer h={50} />
-                    <h1 className="title">HawkHacks 2024</h1>
-                    <p>Laurier | March 8th - 10th | Hybrid</p>
-                    <Spacer h={50} />
-
-                    <p style={{width: "300px", textAlign: "center", color: "var(--black)"}}>
-                        {"We're coming back for another year! "}
-                        <strong>{"Keep up to date with our newsletter below!"}</strong>
-                        {" Wanna be a part of the team? "}
-                        <strong>{"Check out our hiring page below!"}</strong>
-                    </p>
-
-                    <SocialMediaBar />
-                    <Buttons />
-                </Card>
-            </ContentContainer>
-
-            <BackgroundContainer />
-        </AppContainer>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<AppContents />}
+                />
+            </Routes>
+        </BrowserRouter>
     );
 };
+
+const AppContents = () => (
+    <AppContainer>
+        <ContentContainer>
+            {/* <Clouds /> */}
+
+            <a href="https://www.youtube.com/watch?v=cDj2r8QEzzk" target="_blank" rel="noreferrer">
+                <img className="icon" src={Icon} />
+            </a>
+
+            <Card>
+                <Clock />
+                <Spacer h={50} />
+                <h1 className="title">HawkHacks 2024</h1>
+                <p>Laurier | March 8th - 10th | Hybrid</p>
+                <Spacer h={50} />
+
+                <p style={{width: "300px", textAlign: "center", color: "var(--black)"}}>
+                    {"We're coming back for another year! "}
+                    <strong>{"Keep up to date with our newsletter below!"}</strong>
+                    {" Wanna be a part of the team? "}
+                    <strong>{"Check out our hiring page below!"}</strong>
+                </p>
+
+                <SocialMediaBar />
+                <Buttons />
+            </Card>
+        </ContentContainer>
+
+        <BackgroundContainer />
+    </AppContainer>
+);
 
 const AppContainer = ({children}: ContainerProps) => (
     <div className="app-container">
